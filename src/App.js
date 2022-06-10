55d809d757925c86d45dfd2f6914f40aafa7cceb
Oluwatobi-PM <rajioluwatobi3@gmail.com>
2022-06-11 00:42:39 +0100

    all set for auth and db

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './Components/Navbar';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from './Components/PrivateRoute';
 
 
 function App() {
@@ -15,13 +18,17 @@ function App() {
         <Routes>
           <Route path='/' element = {<Explore />} />
           <Route path='/offers' element = {<Offers />} />
-          <Route path='/profile' element = {<Signin />} />
+          <Route path='/profile' element = {<PrivateRoute />} >
+            <Route path='/profile' element = {<Profile />} />
+          </Route>
           <Route path='/sign-in' element = {<Signin />} />
           <Route path='/sign-up' element = {<SignUp />} />
-          <Route path='/forgot-paswword' element = {<ForgotPassword />} />
+          <Route path='/forgot-password' element = {<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
